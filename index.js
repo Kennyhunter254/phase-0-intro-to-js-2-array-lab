5acd3286392a584c200b4f2014ef6860d5953278
Kennyhunter254 <kipronokevin566@gmail.com>
2024-06-29 16:18:52 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+// index.js
+let cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
